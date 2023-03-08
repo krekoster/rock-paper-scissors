92cbba6f617f6e34c45295c4a8d299c92ac817c5
krekoster <72513456+krekoster@users.noreply.github.com>
2023-03-08 20:58:26 +0100

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,61 +1,152 @@
 let computerScore = 0;
 let humanScore = 0;
+let playerChoice = "";
+let playerSelection = 0;
+let computerSelection = 0;
+let statusMessage = "";
 
-function getComputerChoice () {
+function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3);
-    if (computerChoice == 0) {return ("rock");}
-    else if (computerChoice == 1) {return("paper");}
-    else {return("scissors");}
+    if (computerChoice == 0) { return ("rock"); }
+    else if (computerChoice == 1) { return ("paper"); }
+    else { return ("scissors"); }
+}
+function startGame() {
+    // let computerScore = 0;
+    // let humanScore = 0;
+    document.getElementById("popUpWindow").style.display = "none";
+    statusMessage = "CHOOSE YOUR WEAPON!";
+    document.getElementById("statusText").innerHTML = statusMessage;
+    document.getElementById("resultText").innerHTML = humanScore + " : " + computerScore;
+    playGame();
+}
+function playGame() {
+    document.querySelector("#humanButtonRock").addEventListener("click", () => playRound("rock"));
+    document.querySelector("#humanButtonPaper").addEventListener("click", () => playRound("paper"));
+    document.querySelector("#humanButtonScissors").addEventListener("click", () => playRound("scissors"));
+}
+function playRound(playerSelection) {
+    const computerSelection = getComputerChoice();
+    if (playerSelection == "rock" && computerSelection == "rock") {
+        document.getElementById("humanButtonRock").style.transform = "scale(1.3,1.3)";
+        document.getElementById("compButtonRock").style.transform = "scale(1.3,1.3)";
+        document.getElementById("compButtonRock").style.opacity = "1";
+        statusMessage = "It's a tie!";
+        setTimeout(processStatus, 200);
+    }
+    if (playerSelection == "rock" && computerSelection == "paper") {
+        computerScore++;
+        document.getElementById("humanButtonRock").style.transform = "scale(1.3,1.3)";
+        document.getElementById("compButtonPaper").style.transform = "scale(1.3,1.3)";
+        document.getElementById("compButtonPaper").style.opacity = "1";
+        statusMessage = "Paper beats rock!";
+        setTimeout(processStatus, 200);
+    }
+    if (playerSelection == "rock" && computerSelection == "scissors") {
+        humanScore++;
+        document.getElementById("humanButtonRock").style.transform = "scale(1.3,1.3)";
+        document.getElementById("compButtonScissors").style.transform = "scale(1.3,1.3)";
+        document.getElementById("compButtonScissors").style.opacity = "1";
+        statusMessage = "Rock beats scissors!";
+        setTimeout(processStatus, 200);
+    }
+    if (playerSelection == "paper" && computerSelection == "scissors") {
+        computerScore++;
+        document.getElementById("humanButtonPaper").style.transform = "scale(1.3,1.3)";
+        document.getElementById("compButtonScissors").style.transform = "scale(1.3,1.3)";
+        document.getElementById("compButtonScissors").style.opacity = "1";
+        statusMessage = "Scissors beat paper!";
+        setTimeout(processStatus, 200);
+    }
+    if (playerSelection == "paper" && computerSelection == "rock") {
+        humanScore++;
+        document.getElementById("humanButtonPaper").style.transform = "scale(1.3,1.3)";
+        document.getElementById("compButtonRock").style.transform = "scale(1.3,1.3)";
+        document.getElementById("compButtonRock").style.opacity = "1";
+        statusMessage = "Paper beats rock!";
+        setTimeout(processStatus, 200);
+    }
+    if (playerSelection == "paper" && computerSelection == "paper") {
+        document.getElementById("humanButtonPaper").style.transform = "scale(1.3,1.3)";
+        document.getElementById("compButtonPaper").style.transform = "scale(1.3,1.3)";
+        document.getElementById("compButtonPaper").style.opacity = "1";
+        statusMessage = "It's a tie!";
+        setTimeout(processStatus, 200);
+    }
+    if (playerSelection == "scissors" && computerSelection == "paper") {
+        humanScore++;
+        document.getElementById("humanButtonScissors").style.transform = "scale(1.3,1.3)";
+        document.getElementById("compButtonPaper").style.transform = "scale(1.3,1.3)";
+        document.getElementById("compButtonPaper").style.opacity = "1";
+        statusMessage = "Scissors beat paper!";
+        setTimeout(processStatus, 200);
+    }
+    if (playerSelection == "scissors" && computerSelection == "rock") {
+        computerScore++;
+        document.getElementById("humanButtonScissors").style.transform = "scale(1.3,1.3)";
+        document.getElementById("compButtonRock").style.transform = "scale(1.3,1.3)";
+        document.getElementById("compButtonRock").style.opacity = "1";
+        statusMessage = "Rock beats scissors!";
+        setTimeout(processStatus, 200);
+    }
+    if (playerSelection == "scissors" && computerSelection == "scissors") {
+        document.getElementById("humanButtonScissors").style.transform = "scale(1.3,1.3)";
+        document.getElementById("compButtonScissors").style.transform = "scale(1.3,1.3)";
+        document.getElementById("compButtonScissors").style.opacity = "1";
+        statusMessage = "It's a tie!";
+        setTimeout(processStatus, 200);
+    }
+}
+function processStatus() {
+    document.getElementById("statusText").innerHTML = statusMessage;
+    setTimeout(processScore, 200);
+}
+function processScore() {
+    document.getElementById("resultText").innerHTML = humanScore + " : " + computerScore;
+    countRounds();
+}
+function countRounds() {
+    if ((humanScore < 3) && (computerScore < 3)) {
+        setTimeout(resetGui, 300);
+    }
+    else {
+        setTimeout(gameOverMessage, 600);
+    }
+}
+function gameOverMessage() {
+    statusMessage = "game over!";
+    document.getElementById("statusText").innerHTML = statusMessage;
+    setTimeout(resetGui, 600);
+    setTimeout(showPopupWindow, 1000);
+}
+function resetGui() {
+    let buttonsClass = document.getElementsByClassName("buttons");
+    for (let i = 0; i < buttonsClass.length; i++) {
+        buttonsClass[i].style.transform = "";
+        buttonsClass[i].style.opacity = "";
+    }
+}
+function showPopupWindow() {
+    document.getElementById("popUpWindow").style.display = "block";
+    if (humanScore > computerScore) {
+        statusMessage = "Human won!";
+    }
+    else {
+        statusMessage = "Computer won!";
+    }
+    document.getElementById("statusText").innerHTML = statusMessage;
+}
+function fadeToBlack() {
+    document.getElementById("popUpWindow").style.display = "none";
+    document.body.style.backgroundColor = "#2e3840";
+    document.getElementById("content").style.backgroundColor = "#2e3840";
+    
+    document.getElementById("status").style.backgroundColor = "#2e3840";
+    document.getElementById("scoreTable").style.backgroundColor = "#2e3840";
+    document.getElementById("buttonsHolder").style.backgroundColor = "#2e3840";
+    // document.getElementsByTagName("body").style.backgroundColor= "#2e3840";
+
 }
 
-function playRound (playerSelection, computerSelection) {
-    if (playerSelection == "rock" && computerSelection == "rock"){
-        return ("tie");
-    }
-    else if (playerSelection == "rock" && computerSelection == "paper"){
-        computerScore += 1;
-        return ("point for computer, paper beats rock"); 
-    }
-    else if (playerSelection == "rock" && computerSelection == "scissors"){
-        humanScore +=1;
-        return ("point for you, rock beats scissors");
-    }
-    else if (playerSelection == "paper" && computerSelection == "scissors"){
-        computerScore += 1;
-        return ("point for computer, scissors beats paper");
-    }
-    else if (playerSelection == "paper" && computerSelection == "rock"){
-        humanScore +=1;
-        return ("point for you, paper beats rock");
-    }
-    else if (playerSelection == "paper" && computerSelection == "paper"){
-        return ("tie");
-    }
-    else if (playerSelection == "scissors" && computerSelection == "paper"){
-        humanScore +=1;
-        return ("point for you, scissors beats paper");
-    }
-    else if (playerSelection == "scissors" && computerSelection == "rock"){
-        computerScore += 1;
-        return ("point for computer, rock beats scissors");
-    }
-    else if (playerSelection == "scissors" && computerSelection == "scissors"){
-        return ("tie");
-    }
-}
+startGame();
 
-function game () {
-    for (let i = 0; i < 5; i++ ) {
-        //const playerSelection =  prompt("Choose rock, paper or scissors!");
-        playerSelection = "rock";
-        const computerSelection = getComputerChoice();
-        playRound(playerSelection,computerSelection);
-        //console.log ("computerScore je", computerScore);
-        //console.log ("humanScore je", humanScore);
-    }
-}
-game ();
-console.log ("computer Score is", computerScore);
-console.log ("human Score is", humanScore);
-if (computerScore > humanScore) {console.log ("computer wins")}
-else { console.log ("Human wins")};
